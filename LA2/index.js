6d0e930d38647f51e5c414b2d467eefe0cb5254d
vishal898 <vishal.borse898@gmail.com>
2021-10-20 11:19:34 +0530

    adding

--- LA2/index.js
+++ LA2/index.js
@@ -1,5 +1,6 @@
 document.querySelector(".Button").addEventListener("click",myClick);
 
+
 function myClick(event)
 {
     var name=document.querySelector(".name").value;
@@ -18,3 +19,21 @@ function myClick(event)
     alert("Enter valid data");
 }
 
+function myFunction() {
+    var x = document.getElementById("contactb");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunctionv() {
+    var x = document.getElementById("videor");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
